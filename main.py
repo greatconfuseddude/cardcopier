e3ea75141e03aab1de1e00d946abda0b0c3df3c3
mcamp <33528796+greatconfuseddude@users.noreply.github.com>
2023-10-12 11:54:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,13 @@ def copy_files(title, backup_folder_path, sd_folder_path, date):
     for file_name in tqdm(os.listdir(sd_card_path),
         mininterval=2,
         desc="Photo backup is in progress"):
-      file_path = os.path.join(sd_card_path, file_name)
-      new_file_name = title + "_" + date + "_RAW_" + file_name
-      shutil.copy(file_path, os.path.join(backup_folder_path, new_file_name))
+      try:
+        file_path = os.path.join(sd_card_path, file_name)
+        new_file_name = title + "_" + date + "_RAW_" + file_name
+        shutil.copy(file_path, os.path.join(backup_folder_path, new_file_name))
+      except(IndexError) as e:
+        print("Nuhuh u don't")
+          
     print(f'Files on {sd_card_path} have completed copying')
 
 def backup():
@@ -34,19 +38,25 @@ def backup():
                 "Backup Plus",
                 "My Passport For Mac",
                 "My Passport For Mac Two"]
-  selection_menu = SelectionMenu(drives_list); selection_menu.show()
+  
+  selection_menu = SelectionMenu(drives_list)
+  selection_menu.show()
+  
   attached_drive_path = "/Volumes/"+drives_list[selection_menu.selected_option]
   backup_folder_path = os.path.join(attached_drive_path, title)
   os.mkdir(backup_folder_path)
+  
   print(f"Folder was created at {backup_folder_path}")
 
   with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
     fdate = os.path.getctime("/Volumes/Untitled/DCIM/")
     date = dt.datetime.utcfromtimestamp(fdate).strftime("%Y-%m-%d")
+    
     executor.map(copy_files(title, 
                             backup_folder_path, 
                             sd_folder_path, 
-                            date), sd_folder_path)
+                            date), 
+                 sd_folder_path)
 
 if __name__ == "__main__":
   backup()
