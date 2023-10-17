8679177c4660cfcc62b7fb402e2adfa5af6ea54c
b3n <b3n@b3n.desktop>
2023-10-16 21:26:25 -0500

    added settings

--- main.py
+++ main.py
@@ -1,33 +1,107 @@
+import datetime as dt
+import concurrent.futures
+from tqdm import tqdm
+
+from PyQt6.QtWidgets import * 
+from PyQt6 import QtCore, QtGui 
+from PyQt6.QtGui import * 
+from PyQt6.QtCore import * 
 import qdarktheme
 import threading
 import shutil
 import time
 import json
-import os
-import sys
-import datetime as dt
-import concurrent.futures
-from tqdm import tqdm
-from consolemenu import *
-from consolemenu.items import *
-from PyQt6.QtWidgets import * 
-from PyQt6 import QtCore, QtGui 
-from PyQt6.QtGui import * 
-from PyQt6.QtCore import * 
+import os, sys
 
-with open(os.getcwd() + '/settings.json' ,'r+') as file:
-    file_data = json.load(file)
-    drive_loc = file_data['drive_mnt_loc']
-    sd_loc = file_data['sd_mnt_loc']
+
+def get_path(which_path):
+    with open(os.getcwd() + '/settings.json' ,'r+') as file:
+        file_data = json.load(file)
+        if which_path == 'drive_path':
+            return file_data['drive_mnt_loc']
+        elif which_path == 'sd_path':
+            return file_data['sd_mnt_loc']
+
+
+class SettingsGUI(QMainWindow):
+
+    settings_closed = pyqtSignal()
+
+    def __init__(self):
+        super().__init__()
+        
+        self.setWindowTitle("Settings")
+        self.setFixedSize(400, 200) 
+        self.SettingsUI()
+        
+        save_txt = QLabel(self)
+        save_txt.setText("Default Save Path:")
+        save_txt.setGeometry(50, 10, 150, 20) 
+        
+        save_to_txt = QLabel(self)
+        save_to_txt.setText("Default Save To Path:")
+        save_to_txt.setGeometry(50, 70, 150, 20)
+        
+    def SettingsUI(self): 
+        self.save_button = QPushButton("Save", self) 
+        self.save_button.setGeometry(100, 150, 75, 25) 
+        self.save_button.clicked.connect(self.save_settings)
+        self.save_button.setStyleSheet("background-color : green; color: white")
+        
+        self.cancel_button = QPushButton("Cancel", self) 
+        self.cancel_button.setGeometry(225, 150, 75, 25) 
+        self.cancel_button.clicked.connect(self.cancel)
+        self.cancel_button.setStyleSheet("background-color : grey; color: white")
+        
+        self.save_path = QLineEdit(self)   
+        self.save_path.setGeometry(50, 30, 275, 30)  
+        self.save_path.setText(get_path('sd_path'))  
+        
+        self.select_sd = QPushButton("", self) 
+        self.select_sd.setGeometry(325, 30, 25, 30) 
+        self.select_sd.clicked.connect(self.select_sd_loc)
+        self.select_sd.setStyleSheet("background-color : grey; color: white")
+        self.select_sd.setIcon(QIcon(os.getcwd() + '/img/search.png'))
+        
+        self.save_to_path = QLineEdit(self)   
+        self.save_to_path.setGeometry(50, 90, 275, 30)           
+        self.save_to_path.setText(get_path('drive_path')) 
+        
+        self.select_drive = QPushButton("", self) 
+        self.select_drive.setGeometry(325, 90, 25, 30) 
+        self.select_drive.clicked.connect(self.select_drive_loc)
+        self.select_drive.setStyleSheet("background-color : grey; color: white")
+        self.select_drive.setIcon(QIcon(os.getcwd() + '/img/search.png'))
+        
+    def select_sd_loc(self):
+        dialog = QFileDialog.getExistingDirectory(self, 'Select Directory', self.save_path.text()) + '/'
+        self.save_path.setText(dialog)
+        
+    def select_drive_loc(self):
+        dialog = QFileDialog.getExistingDirectory(self, 'Select Directory', self.save_to_path.text()) + '/'
+        self.save_to_path.setText(dialog)
+        
+    def save_settings(self):
+        with open(os.getcwd() + '/settings.json' ,'r+') as file:
+            file_data = json.load(file)
+            file_data['sd_mnt_loc'] = self.save_path.text()
+            file_data['drive_mnt_loc'] = self.save_to_path.text()
+        with open(os.getcwd() + '/settings.json' ,'w') as file:
+            json.dump(file_data, file) 
+        self.hide()  
+        self.settings_closed.emit()     
+        
+    def cancel(self):
+        self.hide()
+        
   
 class GUI(QMainWindow):   
     def __init__(self): 
         super().__init__() 
   
-        self.setWindowTitle("copycat")   
-        self.setFixedSize(500, 320)  
-        self.setStyleSheet("background-color: #0f1112;")  
-        self.UiComponents()   
+        self.setWindowTitle("CopyCAT!")   
+        self.setFixedSize(500, 320)    
+        self.MainUI()   
 
         self.cat_img = QLabel(self)
         pixmap = QPixmap(os.getcwd() + '/img/center.png')
@@ -50,34 +124,34 @@ class GUI(QMainWindow):
         self.cat_right.setHidden(True)
         
         title = QLabel(self)
-        title.setText("copycat")
+        title.setText("CopyCAT!")
         title.setFont(QFont('Arial', 16))
         title.resize(200, 50)
         
         save_txt = QLabel(self)
-        save_txt.setText("SD Card:")
-        save_txt.setGeometry(150, 30, 100, 20) 
+        save_txt.setText("Save Me:")
+        save_txt.setGeometry(130, 35, 100, 20) 
         
         save_to_txt = QLabel(self)
-        save_to_txt.setText("Backup Drive:")
-        save_to_txt.setGeometry(150, 90, 100, 20)
+        save_to_txt.setText("Save Me To:")
+        save_to_txt.setGeometry(130, 95, 100, 20)
         
         album_tag_txt = QLabel(self)
-        album_tag_txt.setText("Album Title:")
-        album_tag_txt.setGeometry(175, 150, 100, 20)
+        album_tag_txt.setText("Album Tag:")
+        album_tag_txt.setGeometry(175, 155, 100, 20)
         
         self.show() 
   
-    def UiComponents(self): 
+    def MainUI(self): 
         self.save = QComboBox(self)   
-        self.save.setGeometry(150, 55, 200, 30)  
-        sd_folder_path = os.listdir(sd_loc)
-        self.save.addItems(sd_folder_path)  
+        self.save.setGeometry(130, 55, 240, 30)  
+        self.sd_folder_path = os.listdir(get_path('sd_path'))
+        self.save.addItems(self.sd_folder_path)  
         
         self.save_to = QComboBox(self)   
-        self.save_to.setGeometry(150, 115, 200, 30)           
-        attached_drive_list = os.listdir(drive_loc)
-        self.save_to.addItems(attached_drive_list)  
+        self.save_to.setGeometry(130, 115, 240, 30)
+        self.attached_drive_list = os.listdir(get_path('drive_path'))
+        self.save_to.addItems(self.attached_drive_list)  
 
         self.album_tag = QLineEdit(self)
         self.album_tag.setGeometry(175, 175, 150, 30) 
@@ -92,6 +166,23 @@ class GUI(QMainWindow):
         self.pbar.setHidden(True)
         self.pbar.setStyleSheet("QProgressBar::chunk ""{" "background-color : purple;""}")
         
+        self.settings = QPushButton("", self) 
+        self.settings.setGeometry(455, 10, 30, 30) 
+        self.settings.clicked.connect(self.open_settings)
+        self.settings.setIcon(QIcon(os.getcwd() + '/img/settings.png'))
+        
+    def open_settings(self):
+        self.settings_w = SettingsGUI()
+        self.settings_w.settings_closed.connect(self.refresh_paths)
+        self.settings_w.show()
+        
+    def refresh_paths(self):
+        self.save.clear() 
+        self.save_to.clear()
+        self.save.addItems(os.listdir(get_path('sd_path')))
+        self.save_to.addItems(os.listdir(get_path('drive_path')))
+        
+    
     def cat_dance(self):
         self.dance = True
         self.cat_img.setHidden(True)
@@ -111,7 +202,10 @@ class GUI(QMainWindow):
         self.pbar.setHidden(False)
         self.start.setEnabled(False)
         self.start.setHidden(True)
-        self.thread = threading.Thread(target=self.backup, args=(self.album_tag.text(),), daemon=True)
+        self.settings.setEnabled(False)
+        self.save.setEnabled(False)
+        self.save_to.setEnabled(False)
+        self.thread = threading.Thread(target=self.backup, args=(self.album_tag.text(), get_path('sd_path'), get_path('drive_path'),), daemon=True)
         self.thread.start()
         self.catdance = threading.Thread(target=self.cat_dance, daemon=True)
         self.catdance.start()
@@ -120,11 +214,14 @@ class GUI(QMainWindow):
         self.dance = False  
         self.pbar.setHidden(True)
         self.pbar.setValue(0)
+        self.settings.setEnabled(True)
+        self.save.setEnabled(True)
+        self.save_to.setEnabled(True)
         self.start.setEnabled(True)
         self.start.setHidden(False)
         self.album_tag.setText("")        
                 
-    def backup(self, title):
+    def backup(self, title, sd_loc, drive_loc):
         self.start.setEnabled(False)
         if title == '':
             title = 'New_Album'
@@ -160,9 +257,10 @@ class GUI(QMainWindow):
                 print("Nuhuh u don't")
           
         print(f'Files on {sd_folder_path} have completed copying')
+        
 
 if __name__ == "__main__":    
     App = QApplication(sys.argv)
     qdarktheme.setup_theme()   
     gui = GUI()  
-    sys.exit(App.exec())
+    sys.exit(App.exec()) 
