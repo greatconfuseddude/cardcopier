60a4891835e83a4272d963d70520c9d55fb0c90c
mcamp <33528796+greatconfuseddude@users.noreply.github.com>
2023-10-19 15:52:34 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-pip install -r requirements.txt
+INSTALL_LOC='/opt/copycat'
+
+echo "SUDO required for install"
+sudo apt install python3-venv -y
+sudo mkdir -p $INSTALL_LOC/{tmp,img}
+sudo cp -r * $INSTALL_LOC
+sudo python3 -m venv $INSTALL_LOC/.venv
+sudo $INSTALL_LOC/.venv/bin/python3 -m pip install -r $INSTALL_LOC/requirements.txt
+sudo chown -R $USER:$USER $INSTALL_LOC
+
 echo 'Python installation succeeded.'
 
 cd ..
