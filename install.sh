2794ce3f4b9bf07e57e042b4a72e3d9b2e1235de
mistercamp <148037744+mistercamp@users.noreply.github.com>
2023-10-19 19:12:05 -0500

    Update install.sh
    updated install

--- install.sh
+++ install.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
 INSTALL_LOC='/opt/copycat'
+ALIAS='alias copycat="'${INSTALL_LOC}'/.venv/bin/python3 '${INSTALL_LOC}'/__main__.py"'
 
 echo "SUDO required for install"
 sudo apt install python3-venv -y
-sudo mkdir -p $INSTALL_LOC/{tmp,img}
+sudo mkdir -p $INSTALL_LOC/img
 sudo cp -r * $INSTALL_LOC
 sudo python3 -m venv $INSTALL_LOC/.venv
 sudo $INSTALL_LOC/.venv/bin/python3 -m pip install -r $INSTALL_LOC/requirements.txt
 sudo chown -R $USER:$USER $INSTALL_LOC
 
-echo 'Python installation succeeded.'
+echo 'Python environment installation succeeded.'
 
-cd ..
-python3 copycat
+sudo rm ${INSTALL_LOC}/requirements.txt ${INSTALL_LOC}/install.sh
+
+if ! grep -Fxq "$ALIAS" ~/.bashrc
+then
+echo  $ALIAS >> ~/.bashrc
+fi
+
+source ~/.bashrc
+
+echo 'Copycat installation succeeded.'
+echo 'To launch app, simply type: copycat'
+
+${INSTALL_LOC}/.venv/bin/python3 ${INSTALL_LOC}/__main__.py
